54763c2bd8be90650369f18a1dd97c621d324200
S.B. Verhoek <S.B.Verhoek@student.tue.nl>
2018-01-12 16:57:27 +0100

    Change forms

--- website/forms.py
+++ website/forms.py
@@ -42,7 +42,7 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
     birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
-    password1 = forms.CharField(help_text='-1 \n -2')
+    #password1 = forms.CharField(help_text="")
     class Meta:
         model = User
         #fields = ('username', 'birth_date', 'password1', 'password2', )
